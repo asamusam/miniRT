834c67e0a266c1a2ff5217c5c50640278462be13
Luke Lai <llai@c3r5s1.42london.com>
2024-05-05 02:19:13 +0100

    Try fixing camera rotation but could not

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: llai <llai@student.42london.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 13:43:24 by llai              #+#    #+#             */
-/*   Updated: 2024/05/04 21:39:56 by llai             ###   ########.fr       */
+/*   Updated: 2024/05/05 02:09:59 by llai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minirt.h"
@@ -14,13 +14,24 @@
 
 void	draw_scene(t_data *data);
 
+t_tuple	camera_up(t_tuple default_up, t_tuple rotation)
+{
+	t_tuple	right;
+	t_tuple	n_right;
+	t_tuple	update_up;
+
+	right = cross(default_up, rotation);
+	n_right = normalize(right);
+	update_up = cross(rotation, n_right);
+	return (update_up);
+}
+
 void	sphere_scene(t_data *data)
 {
 	// t_tuple	from = point(0, 0, -5);
 	t_tuple	from = data->scene->camera.position;
 	t_tuple	to = point(0, 0, 0);
-	// t_tuple	up = vector(0, 1, 0);
-	t_tuple	up = data->scene->camera.rotation;
+	t_tuple	up = vector(0, 1, 0);
 	data->scene->camera.transform = view_transform(from, to, up);
 	render(data, data->scene->camera, data->scene->world);
 	printf("done\n");
